5fee9d66adb3af77db779e57fb70626fda2caf4e
bertw2002 <awan00@stuy.edu>
2019-03-29 10:11:25 -0400

    made function names

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,1 +1,36 @@
+public class MyLinkedList{
+  //    not REQUIRED, but it would be crazy of you not to have this
+  public String toString(){
 
+  }
+
+//construct an empty list
+  public MyLinkedList(){
+
+  }
+
+
+  //  reset the list to an empty state. Very similar to the constructor.
+  public void clear(){
+
+  }
+
+
+  //add an element to the end of the list (the boolean would be true all the time if you want to conform to list standards)
+  public boolean/void add(E){
+
+  }
+
+
+  /*       in O(1) time, connect the other list to the end of this list.
+        The other list is then reset to size 0 (do not wipe out the nodes, just disconnect them.)
+        This is how you will merge lists together for your radix sort.*/
+  public void extend(MyLinkedList<E> other){
+
+  }
+  //remove the 1st element of the list, and return that value.
+  public E removeFront(){
+    
+  }
+
+}
