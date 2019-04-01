ffd0d41bcc80a09b88dbbfd2294cfbb6447b28af
bertw2002 <awan00@stuy.edu>
2019-03-31 21:22:44 -0400

    coded Node

--- Node.java
+++ Node.java
@@ -1,1 +1,34 @@
+public class Node<E>{
+  private E data;
+  private Node<E> next,prev;
+  public Node(E data, Node<E> next, Node<E> prev){
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+  }
+  public Node (E datatwo){
+    data = datatwo;
+  }
+  public Node<E> next(){
+    return next;
+  }
+  public Node<E> prev(){
+    return prev;
+  }
+  public void setNext(Node<E> nexttwo){
+    next = nexttwo;
+  }
+  public void setPrev(Node<E> prevtwo){
+    prev = prevtwo;
+  }
+  public E getData(){
+    return data;
+  }
+  public void setData(E datatwo){
+    data = datatwo;
+  }
+  public String toString(){
+  	return data + "";
+  }
 
+}
