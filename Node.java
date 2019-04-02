e9cad0fc7285b21b86cf2c9f00dc1493f8e768ff
bertw2002 <awan00@stuy.edu>
2019-04-02 11:38:01 -0400

    changed node back to E type

--- Node.java
+++ Node.java
@@ -1,31 +1,30 @@
-public class Node{
+public class Node<E>{
   private E data;
-  private Node next,prev;
-  public Node(int data, Node next, Node prev){
+  private Node<E> next,prev;
+  public Node(E data, Node<E> next, Node<E> prev){
     this.data = data;
     this.next = next;
     this.prev = prev;
   }
-  public Node(int datatwo){
+  public Node (E datatwo){
     data = datatwo;
-
   }
-  public Node next(){
+  public Node<E> next(){
     return next;
   }
-  public Node prev(){
+  public Node<E> prev(){
     return prev;
   }
-  public void setNext(Node nexttwo){
+  public void setNext(Node<E> nexttwo){
     next = nexttwo;
   }
-  public void setPrev(Node prevtwo){
+  public void setPrev(Node<E> prevtwo){
     prev = prevtwo;
   }
   public E getData(){
     return data;
   }
-  public void setData(Integer datatwo){
+  public void setData(E datatwo){
     data = datatwo;
   }
   public String toString(){
